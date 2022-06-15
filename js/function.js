1cf95cd0bd78a2ff851d2589e385ae3755f730fb
mohamadali afsharnia <mohamadali.afsharnia@gmail.com>
2022-06-15 20:03:04 +0430

    The End

---
+++ js/function.js
@@ -0,0 +1,7 @@
+function ph1_changesrc(){
+    document.getElementById('ph1ca_img').src = "media/logo-2.png";
+}
+
+function ph1_returnsrc() {
+    document.getElementById('ph1ca_img').src = "media/logo-1.png";
+}
